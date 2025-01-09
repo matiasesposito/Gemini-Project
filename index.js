aed6235e38fab9f040e335cced0488a0db5f205d
Matias Esposito <matiasesposito.10@hotmail.com>
2025-01-09 11:41:49 -0300

    .

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ import {
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
-    const text = response.text();
-    console.log(text);
+    const respuesta = response.text();
+    
+    typeWriter(respuesta, "typewriter");
   }
 
   run();
--- index.php
+++ index.php
@@ -4,10 +4,47 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <script src="typeWriter.js"></script>
 </head>
+
+<style>
+  .blinking-cursor {
+  margin-left: 5px;
+  background-color: #fff;
+  animation: blink 1s infinite;
+  }
+
+  @keyframes blink {
+    0%, 50% {
+      opacity: 1;
+    }
+    50.1%, 100% {
+      opacity: 0;
+    }
+  }
+
+  pre {
+  white-space: pre-wrap;       /* css-3 */
+  white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+  white-space: -pre-wrap;      /* Opera 4-6 */
+  white-space: -o-pre-wrap;    /* Opera 7 */
+  word-wrap: break-word;       /* Internet Explorer 5.5+ */
+  }
+</style>
 
 <body>
 
+<div>
+  <pre id="typewriter"></pre>
+</div>
+
+
+
+
+
+
+  <script type="text/javascript" src="main.js"></script>
   <script type="importmap">
     {
     "imports": {
