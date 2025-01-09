2d13486da0a388f2503795f535fad23a8987ea43
Matias Esposito <matiasesposito.10@hotmail.com>
2025-01-09 11:50:07 -0300

    .

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
   white-space: -pre-wrap;      /* Opera 4-6 */
   white-space: -o-pre-wrap;    /* Opera 7 */
   word-wrap: break-word;       /* Internet Explorer 5.5+ */
+  font-family: "Helvetica", Helvetica, Arial, sans-serif;
   }
 </style>
 
