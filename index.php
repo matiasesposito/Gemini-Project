7a52c570e15eca14b0f2fa3a50f41d371dfa0f42
Matias Esposito <matiasesposito.10@hotmail.com>
2025-02-12 11:40:52 -0300

    Mejorar estilos de tarjetas y actualizar estructura HTML para cargar evaluadores

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  
+
   <link rel="stylesheet" href="styles.css">
   <title>Menu</title>
 </head>
@@ -16,46 +16,46 @@
 <body>
 
 
-<div class="container-xl h-100 d-flex flex-column align-items-stretch justify-content-center">
-      <div class="row justify-content-between gx-5">
-        <div class="col-4">
-          <a href="cargar_evaluador/cargar.php">
-            <div class="card" id="cargarEvaluador">
-                <div class="content">
-                  <div class="header">
-                    <span class="tittle">Cargar evaluador</span>
-                  </div>
-                  <p class="desc">Dar de alta un nuevo evaluador y subir su CV</p>
-                </div>
+  <div class="container-xl h-100 d-flex flex-column align-items-stretch justify-content-center">
+    <div class="row justify-content-between gx-5">
+      <div class="col-4">
+        <a href="cargar_evaluador/cargar.php">
+          <div class="card text-bg-dark">
+            <img src="img/curriculum-vitae.png" class="card-img p-3" alt="...">
+            <div class="card-img-overlay">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small>Last updated 3 mins ago</small></p>
             </div>
-          </a>
-        </div>
-        <div class="col-4">
-          <a href="buscar/buscar.php">
-            <div class="card" id="buscarEvaluador">
-              <div class="content">
-                <div class="header">
-                  <span class="tittle">Buscar evaluadores</span>
-                </div>
-                <p class="desc">Buscar evaluadores por </p>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-4">
-          <a href="buscar/buscar.php">
-            <div class="card" id="buscarEvaluador">
-              <div class="content">
-                <div class="header">
-                  <span class="tittle">Buscar evaluadores</span>
-                </div>
-                <p class="desc">Buscar evaluadores por </p>
-              </div>
-            </div>
-          </a>
-        </div>
+          </div>
+        </a>
       </div>
-</div>
+      <div class="col-4">
+        <a href="buscar/buscar.php">
+          <div class="card" id="buscarEvaluador">
+            <div class="content">
+              <div class="header">
+                <span class="tittle">Buscar evaluadores</span>
+              </div>
+              <p class="desc">Buscar evaluadores por </p>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-4">
+        <a href="buscar/buscar.php">
+          <div class="card" id="buscarEvaluador">
+            <div class="content">
+              <div class="header">
+                <span class="tittle">Buscar evaluadores</span>
+              </div>
+              <p class="desc">Buscar evaluadores por </p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
 
 
 </body>
