b8b3768347c0046895e04daa2eb08639c967ea4f
Matias Esposito <matiasesposito.10@hotmail.com>
2025-02-12 20:56:25 -0300

    Actualizar estructura HTML y estilos de tarjetas en index.php y styles.css para mejorar la presentación y responsividad

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="styles.css">
   <title>Menu</title>
 </head>
@@ -16,47 +17,48 @@
 <body>
 
 
-  <div class="container-xl h-100 d-flex flex-column align-items-stretch justify-content-center">
-    <div class="row justify-content-between gx-5">
-      <div class="col-4">
+
+
+  <div class="container-xxl h-100 w-100 d-flex flex-column flex-wrap align-items-stretch justify-content-center">
+    <div class="grid text-center">
+      <div class="g-col-12 g-col-md-4 mx-3">
         <a href="cargar_evaluador/cargar.php">
-          <div class="card text-bg-dark text-center">
-            <img src="img/curriculum-vitae.png" class="card-img p-5" alt="...">
-            <div class="card-img-overlay py-5 d-flex flex-column justify-content-between align-items-center">
-              <h3 class="card-title">Cargar Evaluador</h3>
-              <h5 class="card-text">Cargar los datos de un evaluador y subir su CV</h5>
-              <p class="card-text"><small></small></p>
+          <div class="card text-bg-dark text-center h-100">
+            <img src="img/curriculum-vitae.png" class="card-img w-100 position-relative top-50 start-50 translate-middle" alt="...">
+            <div class="card-img-overlay py-4 d-flex flex-column justify-content-center align-items-center">
+              <h2 class="card-title my-5">Cargar Evaluador</h2>
+              <h5 class="card-text my-5">Cargar los datos de un evaluador y subir su CV</h5>
             </div>
           </div>
         </a>
       </div>
-      <div class="col-4">
-        <a href="buscar/buscar.php">
-          <div class="card" id="buscarEvaluador">
-            <div class="content">
-              <div class="header">
-                <span class="tittle">Buscar evaluadores</span>
-              </div>
-              <p class="desc">Buscar evaluadores por </p>
+
+      <div class="g-col-12 g-col-md-4 mx-3">
+        <a href="cargar_evaluador/cargar.php">
+          <div class="card text-bg-dark text-center h-100">
+            <img src="img/find-svgrepo-com.svg" class="card-img w-100 position-relative top-50 start-50 translate-middle" alt="...">
+            <div class="card-img-overlay py-4 d-flex flex-column justify-content-center align-items-center">
+              <h2 class="card-title my-5">Buscar Evaluador</h2>
+              <h5 class="card-text my-5">Buscar evaluadores por</h5>
             </div>
           </div>
         </a>
       </div>
-      <div class="col-4">
-        <a href="buscar/buscar.php">
-          <div class="card" id="buscarEvaluador">
-            <div class="content">
-              <div class="header">
-                <span class="tittle">Buscar evaluadores</span>
-              </div>
-              <p class="desc">Buscar evaluadores por </p>
+
+      <div class="g-col-12 g-col-md-4 mx-3">
+        <a href="cargar_evaluador/cargar.php">
+          <div class="card text-bg-dark text-center h-100">
+            <img src="img/find-svgrepo-com.svg" class="card-img w-100 position-relative top-50 start-50 translate-middle" alt="...">
+            <div class="card-img-overlay py-4 d-flex flex-column justify-content-center align-items-center">
+              <h2 class="card-title my-5">Buscar Evaluador</h2>
+              <h5 class="card-text my-5">Buscar evaluadores por</h5>
             </div>
           </div>
         </a>
       </div>
+
     </div>
   </div>
-
 
 </body>
 
