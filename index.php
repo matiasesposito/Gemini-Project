e5a57947f08cf72bf71f0f223b27152964f7dc9e
Matias Esposito <matiasesposito.10@hotmail.com>
2025-02-20 11:37:58 -0300

    Agregar archivos de fuentes y estilos de Font Awesome 6.7.2; incluir iconos, shims y utilidades para mejorar la presentación y accesibilidad.

--- index.php
+++ index.php
@@ -6,72 +6,62 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- mojocss -->
   <script src="https://unpkg.com/mojocss"></script>
+  <!-- Card css -->
+  <link rel="stylesheet" href="card.css">
+  <!-- Fontawesome -->
+  <link href="librerias/fontawesome-free-6.7.2-web/css/all.css" rel="stylesheet" />
   <title>Menu</title>
 </head>
 
-<style type="text/css">
-
-section .card{
-  background-position: 50% 50%;
-  background-repeat: no-repeat;
-  color: white;
-  transition: all 0.3s ease;
-  cursor: pointer;
-  display: flex;
-  flex-direction: column;
-  row-gap: 45px;
-  align-items: center;
-  padding: 0px 15px;
-  justify-content: center;
-  text-align: center;
-  font-family: Cambria;
-  background-color: rgba(17, 24, 39, 1);
-}
-
-.bg-img-img\/curriculum-vitae2\.png {
-  background-size: calc(80% - 2vw) !important;
-}
-
-.bg-img-img\/find2\.png{
-  background-size: calc(90% - 2vw) !important;
-}
-
-section .card:hover{
-  box-shadow: 0 0rem 1.4rem 0.1rem rgb(225, 227, 230);
-  background-color: rgba(17, 24, 39, 1);
-  scale: 1.05;
-}
-
-
-
-
-</style>
-
-<body style="background: #222639;" class="h-100vh grid rows-10 px-5 py-2">
+<body class="h-100vh grid rows-11 px-5 py-2">
 
   <header class="r-start-1 r-end-4">
     
   </header>
 
-  <article class="r-start-4 r-end-8">
-    <section class="grid py-1 w-80% ma-auto h-100% gap-16 j-items-stretch" md="cols-auto" lg="cols-auto">
-      <a class="bg-img-img/curriculum-vitae2.png card" href="cargar_evaluador/cargar.php">
-        <h2 class="text-xl">Cargar Evaluador</h2>
-        <h5 class="text-md">Cargar los datos de un evaluador y subir su CV</h5>
-      </a>
-      
-      <a class="bg-img-img/find2.png card" href="buscar/buscar.php">
-        <h2 class="text-xl">Buscar Evaluador</h2>
-        <h5 class="text-md">Buscar evaluadores por</h5>
-      </a>
-      <div class="bg-img-img/find2.png card">
-        <h2 class="text-xl">Buscar Evaluador</h2>
-        <h5 class="text-md">Buscar evaluadores por</h5>
+  <article class="r-start-4 r-end-9">
+    <section class="grid py-1 w-80% ma-auto h-100% gap-16 j-items-center" md="cols-auto" lg="cols-auto">
+
+      <div class="card-wrap">
+        <a href="cargar_evaluador/cargar.php">
+          <div class="card-header one">
+            <i class="fa-regular fa-id-badge"></i>
+          </div>
+          <div class="card-content">
+            <h1 class="card-title">Cargar Evaluador</h1>
+            <p class="card-text">Cargar los datos de un evaluador y subir su CV</p>
+          </div>
+        </a>
       </div>
+      
+      <div class="card-wrap">
+        <a href="buscar/buscar.php">
+          <div class="card-header one">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+          <div class="card-content">
+            <h1 class="card-title">Buscar Evaluador</h1>
+            <p class="card-text">Buscar evaluadores por</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="card-wrap">
+        <a href="buscar/buscar.php">
+          <div class="card-header one">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+          <div class="card-content">
+            <h1 class="card-title">Buscar Evaluador</h1>
+            <p class="card-text">Buscar evaluadores por</p>
+          </div>
+        </a>
+      </div>
+
     </section>
   </article>
 
-  <footer class="r-start-8 r-end-11">
+  <footer class="r-start-9 r-end-12">
     
   </footer>
 
