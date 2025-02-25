40664a27ae2e4627eaf7e0e7c67603bf101b9e6b
Matias Esposito <matiasesposito.10@hotmail.com>
2025-02-25 00:51:16 -0300

    Agregar consulta SQL para verificar duplicados de evaluadores basados en coincidencias de nombres.

---
+++ sql_evaluadores_duplicados.sql
@@ -0,0 +1,7 @@
+SET @busqueda = 'Benitez Alejandra Laura';
+
+SELECT *
+FROM evaluadores
+WHERE 
+    ((INSTR(nombre, 'Marcela') > 0) +
+    (INSTR(nombre, 'Laura') > 0)) >= 2;  -- Debe coincidir con al menos 2 palabras
