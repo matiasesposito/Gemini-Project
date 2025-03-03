0f62bf24354d6056ec108d2551e57d5e09df35f8
Matias Esposito <matiasesposito.10@hotmail.com>
2025-03-03 19:29:21 -0300

    Eliminar archivo asd.txt y agregar íconos de Lordicon en index.php

--- index.php
+++ index.php
@@ -10,13 +10,16 @@
   <link rel="stylesheet" href="card.css">
   <!-- Fontawesome -->
   <link href="librerias/fontawesome-free-6.7.2-web/css/all.css" rel="stylesheet" />
+  <!-- lordicon -->
+  <script src="https://cdn.lordicon.com/lordicon.js"></script>
+
   <title>Menu</title>
 </head>
 
 <body class="h-100vh grid rows-11 px-5 py-2">
 
   <header class="r-start-1 r-end-4">
-    
+
   </header>
 
   <article class="r-start-4 r-end-9">
@@ -25,7 +28,14 @@
       <div class="card-wrap">
         <a href="cargar_evaluador/cargar.php">
           <div class="card-header one">
-            <i class="fa-regular fa-id-badge"></i>
+          <lord-icon
+              src="https://cdn.lordicon.com/fjvfsqea.json"
+              trigger="hover"
+              state="hover-unfold"
+              colors="primary:#e4e4e4,secondary:#08a88a"
+              target=".card-wrap"
+              style="width:90%;height:90%">
+          </lord-icon>
           </div>
           <div class="card-content">
             <h1 class="card-title">Cargar Evaluador</h1>
@@ -33,7 +43,10 @@
           </div>
         </a>
       </div>
-      
+
+
+
+
       <div class="card-wrap">
         <a href="buscar/buscar.php">
           <div class="card-header one">
@@ -59,14 +72,61 @@
       </div>
 
     </section>
+
+    <!-- <section class="grid py-1 w-80% ma-auto h-100% gap-16 j-items-center" md="cols-auto" lg="cols-auto">
+      <div class="block">
+        <div class="icon">
+          <lord-icon
+            class="current-color"
+            trigger="hover"
+            target=".block"
+            src="https://cdn.lordicon.com/lbjtvqiv.json"></lord-icon>
+        </div>
+        <div class="card-content">
+          <h1 class="card-title">Buscar Evaluador</h1>
+          <p class="card-text">Buscar evaluadores por</p>
+        </div>
+      </div>
+
+      <div class="block">
+        <div class="icon">
+          <lord-icon
+            class="current-color"
+            trigger="hover"
+            target=".block"
+            src="https://cdn.lordicon.com/lbjtvqiv.json"></lord-icon>
+        </div>
+        <div class="card-content">
+          <h1 class="card-title">Buscar Evaluador</h1>
+          <p class="card-text">Buscar evaluadores por</p>
+        </div>
+      </div>
+
+      <div class="block">
+        <div class="icon">
+          <lord-icon
+            class="current-color"
+            trigger="hover"
+            target=".block"
+            src="https://cdn.lordicon.com/lbjtvqiv.json"></lord-icon>
+        </div>
+        <div class="card-content">
+          <h1 class="card-title">Buscar Evaluador</h1>
+          <p class="card-text">Buscar evaluadores por</p>
+        </div>
+      </div>
+    </section> -->
+
   </article>
 
   <footer class="r-start-9 r-end-12">
-    
+
   </footer>
 
 </body>
 
-<script>mojo();</script>
+<script>
+  mojo();
+</script>
 
 </html>
